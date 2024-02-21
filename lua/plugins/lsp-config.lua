14d9fff5a6cd8c90075959a38d7e96951b2e68fb
Matthew Contos <matt@contos.us>
2024-02-21 14:04:54 -0800

    updated lsp config for commonly used languages

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,6 +11,14 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
+          "gopls",
+          "bashls",
+          "ruby_ls",
+          "rubocop",
+          "terraformls",
+          "tflint",
+          "jsonls",
+          "yamlls"
         }
       })
     end
@@ -20,8 +28,39 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
-      -- lua config
+      -- Lang Configs
+      -- lua
       lspconfig.lua_ls.setup({})
+
+      -- Go
+      lspconfig.gopls.setup({})
+
+      -- Bash
+      lspconfig.bashls.setup({})
+
+      -- Ruby
+      lspconfig.ruby_ls.setup({})
+      lspconfig.rubocop.setup({})
+
+      -- Terraform
+      lspconfig.terraformls.setup({
+        vim.api.nvim_create_autocmd({"BufWritePre"}, {
+          pattern = {"*.tf", "*.tfvars"},
+          callback = function()
+            vim.lsp.buf.format()
+          end
+        })
+      })
+
+      lspconfig.tflint.setup({})
+
+      -- JSON
+      lspconfig.jsonls.setup({})
+
+      -- Yaml
+      lspconfig.yamlls.setup({})
+
+
 
       -- Keymaps
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})

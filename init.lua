0725f2772848c8f17f02bb704fcd24d2e7a33014
Matthew Contos <matt@contos.us>
2024-02-21 10:34:11 -0800

    created base neovim config with bare minimum plugins

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- NeoVim Init Options
+
+-- Plugin Manager (Lazy)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+
+-- Plugin Configuration
+-- Lazy
+require("lazy").setup("plugins")
